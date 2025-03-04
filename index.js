3fd61024d9a9c3b27f4b3ddcc3075557d4714e54
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-04 12:29:55 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
+  if(city == 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
+  
+
   // Write your code here!
+  // if (feet=<400,>=2000){
+  //   return'That will be  twenty bucks.';
+  // }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
